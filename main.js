81fdff234488dd97a219325de6caf0f7fa97c060
jarvisar <jarvisar@mail.uc.edu>
2023-03-20 02:29:38 -0400

    Added konami code secret

--- main.js
+++ main.js
@@ -452,3 +452,17 @@ renderer.domElement.addEventListener('click', function(event) {
 
 
 render();
+
+const konamiCode = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "KeyB", "KeyA"];
+  let konamiIndex = 0;
+
+  document.addEventListener("keydown", function(event) {
+    if (event.code === konamiCode[konamiIndex]) {
+      konamiIndex++;
+      if (konamiIndex === konamiCode.length) {
+        window.location.href = "tetris.html";
+      }
+    } else {
+      konamiIndex = 0;
+    }
+  });
