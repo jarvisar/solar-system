0a86781e80d0feca7ac58724f1230a007a76fa87
jarvisar <jarvisar@mail.uc.edu>
2023-03-26 19:37:44 -0400

    Added city lights and specular map

--- main.js
+++ main.js
@@ -235,7 +235,7 @@ function createPlanets(){
 
   // earth
   const earthGeometry = new THREE.SphereGeometry(20 * scale, 128, 128);
-  const earthMaterial = new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load('public/2k_earth.jpg'), bumpMap: new THREE.TextureLoader().load('public/earth_elevation.jpg'), bumpScale: 0.2 * scale });
+  const earthMaterial = new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load('public/2k_earth.jpg'), bumpMap: new THREE.TextureLoader().load('public/earth_elevation.jpg'), bumpScale: 0.2 * scale, specularMap: new THREE.TextureLoader().load('public/2k_earth_specular_map.tif'), specular: new THREE.Color('grey'), emissive: 0xffffff, emissiveIntensity: 0.2, emissiveMap: new THREE.TextureLoader().load('public/2k_earth_nightmap.jpg') });
   earth = new THREE.Mesh(earthGeometry, earthMaterial);
   earth.castShadow = true;
   earth.receiveShadow = true;
